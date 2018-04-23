72015f3bbb78080ba779b125e2402d519e5c1de7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Holidays_Custom {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler3 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler4 = new DevExpress.XtraScheduler.TimeRuler();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.dateNavigator1 = new DevExpress.XtraScheduler.DateNavigator();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.WorkWeek;
+            this.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.schedulerControl1.Location = new System.Drawing.Point(0, 0);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.OptionsView.ToolTipVisibility = DevExpress.XtraScheduler.ToolTipVisibility.Always;
+            this.schedulerControl1.Size = new System.Drawing.Size(639, 415);
+            this.schedulerControl1.Start = new System.DateTime(2007, 12, 24, 0, 0, 0, 0);
+            this.schedulerControl1.Storage = this.schedulerStorage1;
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler3);
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler4);
+            this.schedulerControl1.CustomDrawDayHeader += new DevExpress.XtraScheduler.CustomDrawObjectEventHandler(this.schedulerControl1_CustomDrawDayHeader);
+            // 
+            // dateNavigator1
+            // 
+            this.dateNavigator1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dateNavigator1.Location = new System.Drawing.Point(639, 0);
+            this.dateNavigator1.Name = "dateNavigator1";
+            this.dateNavigator1.SchedulerControl = this.schedulerControl1;
+            this.dateNavigator1.Size = new System.Drawing.Size(179, 415);
+            this.dateNavigator1.TabIndex = 1;
+            this.dateNavigator1.View = DevExpress.XtraEditors.Controls.DateEditCalendarViewType.MonthInfo;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 415);
+            this.Controls.Add(this.schedulerControl1);
+            this.Controls.Add(this.dateNavigator1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private DevExpress.XtraScheduler.DateNavigator dateNavigator1;
+    }
+}
+
---
+++ CS/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.XtraScheduler;
+using DevExpress.XtraScheduler.Drawing;
+using DevExpress.Schedule;
+
+
+namespace Holidays_Custom {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            schedulerControl1.BeginUpdate();
+            schedulerControl1.WorkDays.Clear();
+            schedulerControl1.WorkDays.Add(DevExpress.XtraScheduler.WeekDays.Sunday | DevExpress.XtraScheduler.WeekDays.Monday
+                  | DevExpress.XtraScheduler.WeekDays.Tuesday | DevExpress.XtraScheduler.WeekDays.Wednesday
+                  | DevExpress.XtraScheduler.WeekDays.Thursday);
+
+            GenerateHolidaysFor2008();
+            schedulerControl1.EndUpdate();
+
+            schedulerControl1.Start = DateTime.Now.Date;
+            schedulerControl1.ActiveViewType = SchedulerViewType.WorkWeek;
+        }
+
+        // A collection of Kuwait Holidays for 2008.
+        private Holiday[] KuwaitHolidays2008 = { 
+            new Holiday(new DateTime(2008, 1, 1), "New Year's Day"),
+            new Holiday(new DateTime(2008, 1, 10), "Hejira New Year (Islamic New Year)"),
+            new Holiday(new DateTime(2008, 2, 24), "Bridge Public Holiday"),
+            new Holiday(new DateTime(2008, 2, 25), "National Day"),
+            new Holiday(new DateTime(2008, 2, 26), "Liberation Day"),
+            new Holiday(new DateTime(2008, 3, 20), "The Prophet's birthday"),
+            new Holiday(new DateTime(2008, 5, 15), "Public Holiday (National mourning for Father Amir"),
+            new Holiday(new DateTime(2008, 5, 16), "Public Holiday (National mourning for Father Amir"),
+            new Holiday(new DateTime(2008, 7, 31), "The Prophet's Ascension"),
+            new Holiday(new DateTime(2008, 9, 30), "Government Holiday"),
+            new Holiday(new DateTime(2008, 10, 1), "Eid al Fitr (End of Ramadan)"),
+            new Holiday(new DateTime(2008, 10, 2), "Eid al Fitr holiday"),
+            new Holiday(new DateTime(2008, 10, 3), "Eid al Fitr holiday"),
+            new Holiday(new DateTime(2008, 10, 4), "Eid al Fitr holiday"),
+            new Holiday(new DateTime(2008, 12, 7), "Eid al Adha holiday"),
+            new Holiday(new DateTime(2008, 12, 8), "Eid al Adha (feast of sacrifice)"),
+            new Holiday(new DateTime(2008, 12, 9), "Eid al Adha holiday"),
+            new Holiday(new DateTime(2008, 12, 10), "Eid al Adha holiday"),
+            new Holiday(new DateTime(2008, 12, 11), "Eid al Adha holiday"),
+            new Holiday(new DateTime(2008, 12, 29), "Hejira New Year (Islamic New Year)"),
+        };
+
+        // This method adds holidays from the KuwaitHolidays2007 collection
+        // to the WorkDays collection of the Scheduler Control.
+        private void GenerateHolidaysFor2008() {
+            foreach (Holiday item in KuwaitHolidays2008) {
+                this.schedulerControl1.WorkDays.Add(item);
+            }
+        }
+
+        private void schedulerControl1_CustomDrawDayHeader(object sender, CustomDrawObjectEventArgs e) {
+            // Check whether the current object is a Day Header.
+            SchedulerHeader header = e.ObjectInfo as SchedulerHeader;
+            if (header != null) {
+                // Check whether the current date is a known holiday.
+                Holiday hol = FindHoliday(header.Interval.Start.Date);
+                if (hol != null) {
+                    // Add the holiday name to the day header's caption.
+                    header.Caption = String.Format("{0} ({1})", hol.DisplayName, hol.Date.ToShortDateString());
+                }
+            }
+        }
+
+        // This method finds a holiday for the specified date.
+        private Holiday FindHoliday(DateTime date) {
+            foreach (WorkDay item in schedulerControl1.WorkDays) {
+                if (item is Holiday) {
+                    Holiday hol = (Holiday)item;
+                    if (hol.Date == date)
+                        return hol;
+                }
+            }
+            return null;
+        }
+    }
+}
