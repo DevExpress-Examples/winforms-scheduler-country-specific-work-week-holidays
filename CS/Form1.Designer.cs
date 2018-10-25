77242de5d785415d3d0193cc2fb6b035852bb031
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-10-25 17:39:04 +0300

    initial commit

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace DisplayCustomHolidays {
+    partial class Form1 : DevExpress.XtraEditors.XtraForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler1 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler2 = new DevExpress.XtraScheduler.TimeRuler();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.dateNavigator1 = new DevExpress.XtraScheduler.DateNavigator();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1.CalendarTimeProperties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.WorkWeek;
+            this.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.schedulerControl1.Location = new System.Drawing.Point(0, 0);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.OptionsView.ToolTipVisibility = DevExpress.XtraScheduler.ToolTipVisibility.Always;
+            this.schedulerControl1.Size = new System.Drawing.Size(588, 415);
+            this.schedulerControl1.Start = new System.DateTime(2015, 2, 23, 0, 0, 0, 0);
+            this.schedulerControl1.Storage = this.schedulerStorage1;
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            this.schedulerControl1.Views.DayView.AllDayAreaScrollBarVisible = false;
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1);
+            this.schedulerControl1.Views.FullWeekView.AllDayAreaScrollBarVisible = false;
+            this.schedulerControl1.Views.TimelineView.TimelineScrollBarVisible = false;
+            this.schedulerControl1.Views.WorkWeekView.AllDayAreaScrollBarVisible = false;
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2);
+            this.schedulerControl1.CustomDrawDayHeader += new DevExpress.XtraScheduler.CustomDrawObjectEventHandler(this.schedulerControl1_CustomDrawDayHeader);
+            // 
+            // dateNavigator1
+            // 
+            this.dateNavigator1.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            //this.dateNavigator1.CellPadding = new System.Windows.Forms.Padding(2);
+            this.dateNavigator1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dateNavigator1.FirstDayOfWeek = System.DayOfWeek.Sunday;
+            this.dateNavigator1.Location = new System.Drawing.Point(588, 0);
+            this.dateNavigator1.Name = "dateNavigator1";
+            this.dateNavigator1.SchedulerControl = this.schedulerControl1;
+            this.dateNavigator1.Size = new System.Drawing.Size(230, 415);
+            this.dateNavigator1.TabIndex = 1;
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Office 2013";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 415);
+            this.Controls.Add(this.schedulerControl1);
+            this.Controls.Add(this.dateNavigator1);
+            this.Name = "Form1";
+            this.Text = "Display Custom Holidays";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private DevExpress.XtraScheduler.DateNavigator dateNavigator1;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,16 @@
+# How to display a custom work week and holidays
+
+
+<p>Problem:</p>
+<p>In Kuwait weekly holidays are Friday and Saturday. Sunday to Thursday are working days. How can I implement this in the XtraScheduler? Also I'd like to show official holidays in the Scheduler and in the DateNavigator and highlight them.</p>
+<p>Solution:</p>
+<p>You should use the <a href="http://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraSchedulerWorkDaysCollectiontopic">SchedulerControl.WorkDays</a> collection to accomplish this task. Add the required weekdays and holidays to the collection. <br>To change the highlight color of the date in the <a href="http://help.devexpress.com/#WindowsForms/CustomDocument1740">DateNavigator control</a>, handle its <strong>CustomDrawDayNumberCell</strong> event or specify a custom cell style provider with the <strong>DateNavigator.CellStyleProvider</strong> property for version 15.2 and higher.<br> To change the header captions for holidays handle the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraSchedulerSchedulerControl_CustomDrawDayHeadertopic">SchedulerControl.CustomDrawDayHeader</a> event. Consider also using <a href="http://documentation.devexpress.com/#WindowsForms/CustomDocument4747">Formatting Services</a> as an alternative technique.<br><br>The application looks like as shown below.<br><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-display-a-custom-work-week-and-holidays-e27/15.2.4+/media/5f1d0e44-95d3-11e5-80bf-00155d62480c.png"></p>
+
+
+<h3>Description</h3>
+
+Create the&nbsp;<strong>CustomCellStyleProvider</strong> class which implements the &nbsp;<strong>ICalendarCellStyleProvider</strong> interface. The&nbsp;<strong>UpdateAppearance</strong> method changes cell colors as required. Use the <strong>DateNavigator.CellStyleProvider</strong> property to specify the instance of a&nbsp;custom style provider&nbsp;that will be used to color the cells and display icons.
+
+<br/>
+
+
